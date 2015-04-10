cf4f7d71952af3ae680245af8155b09a1cdd830d
Jemond Patterson <pattersonjemond@gmail.com>
2015-04-10 10:38:32 -0400

    Created estuk application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estuk::Application.config.secret_token = '059473d87946b1b46f2962719caf66535adcb0176f08ef869c5d0acb1d6997ddb917e9e0fdf0bf8e07f74871c61916e6fa17cde2e7064795fb6a25fe52bd2b3e'
